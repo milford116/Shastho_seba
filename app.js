1c68070efb2466c3a6355feda95ba62e4a04f104
return-100 <imransci143@gmail.com>
2020-04-14 22:20:30 +0600

    initialize backend for doktor app

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+dotenv.config();
+
+mongoose.set('useCreateIndex', true);
+
+const options = {
+    useUnifiedTopology: true, 
+    useNewUrlParser: true
+}
+
+mongoose.connect(process.env.DB_URL, options, (err)=>{
+    if (!err)
+        console.log("Successfully connected to database");
+    else
+        console.log("Error occurred during database connection");
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+//add your route here
+
+app.listen(process.env.PORT, ()=>
+{
+    console.log("Server started at port " + process.env.PORT);
+});

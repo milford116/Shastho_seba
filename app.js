1e69f84e161cca804c2f94e35f46b04384c2a27d
return-100 <imransci143@gmail.com>
2020-04-15 03:26:24 +0600

    simple example of jwt has been done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 const cors = require("cors");
 dotenv.config();
 
@@ -11,6 +12,11 @@ mongoose.set('useCreateIndex', true);
 const options = {
     useUnifiedTopology: true, 
     useNewUrlParser: true
+}
+
+const payload = {
+    name: "Imran",
+    pass: "123456"
 }
 
 mongoose.connect(process.env.DB_URL, options, (err)=>{
@@ -26,6 +32,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 //add your route here
+app.get("/token", (req, res)=>{
+    const token = jwt.sign(payload, process.env.SECRET);
+    res.send(token);
+});
 
 app.listen(process.env.PORT, ()=>
 {

67ff8abde9c51576f1ae35bd139d6ef9b59c587c
return-100 <imransci143@gmail.com>
2020-04-21 02:36:49 +0600

    jwt example removed

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 dotenv.config();
+const patientRoutes = require("./routes/patient");
 
 mongoose.set('useCreateIndex', true);
 
 const options = {
     useUnifiedTopology: true, 
     useNewUrlParser: true
-}
-
-const payload = {
-    name: "Imran",
-    pass: "123456"
 }
 
 mongoose.connect(process.env.DB_URL, options, (err)=>{
@@ -32,11 +28,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 //add your route here
-app.get("/token", (req, res)=>{
-    const token = jwt.sign(payload, process.env.SECRET);
-    res.send(token);
-});
-const patientRoutes = require("./routes/patient");
 app.use(patientRoutes);
 
 app.listen(process.env.PORT, ()=>

2852369fab6dd6ee0a3c07bcc7fa83719b2da836
Waqar-107 <waqar.hassan866@gmail.com>
2020-04-21 13:35:36 +0600

    login bug fixed and tested using postman

--- routes/doctor.js
+++ routes/doctor.js
@@ -2,7 +2,7 @@ const doctorController = require('../controllers/doctor.controller');
 const express = require('express');
 const router = express.Router();
 
-// router.post('/doctor/login', patientController.login);
+router.post('/doctor/login', doctorController.login);
 router.post('/doctor/register', doctorController.registration);
 
 module.exports = router;

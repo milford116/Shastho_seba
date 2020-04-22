b0ebc9a79b1209dddec92007bd758db046f6fdba
return-100 <imransci143@gmail.com>
2020-04-22 16:13:47 +0600

    slight change in appointment model

--- models/transaction.model.js
+++ models/transaction.model.js
@@ -2,16 +2,10 @@ const mongoose = require("mongoose");
 mongoose.pluralize(null);
 
 var transactionSchema = new mongoose.Schema({
-	txn_id: {
-		type: String,
-		required: true,
-	},
-
 	appointment_id: {
-		type: String,
+		type: ObjectId,
 		required: true,
 	},
-
 	amount: {
 		type: Number,
 		required: true,

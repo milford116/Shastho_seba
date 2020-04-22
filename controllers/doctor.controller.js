816bcf1b28c9e3cd036ec7e02f5caff955422f1b
Waqar-107 <waqar.hassan866@gmail.com>
2020-04-22 13:50:13 +0600

    error msg updated

--- controllers/doctor.controller.js
+++ controllers/doctor.controller.js
@@ -10,11 +10,11 @@ const {SUCCESS, INTERNAL_SERVER_ERROR, BAD_REQUEST, DATA_NOT_FOUND} = require(".
 exports.registration = async function (req, res) {
 	doctorModel.findOne({mobile_no: req.body.mobile_no}, (err, docs) => {
 		if (docs) {
-			res.status(BAD_REQUEST).send("An account with this mobile no already exists");
+			res.status(BAD_REQUEST).send("An account with this mobile no. already exists");
 		} else {
 			referenceModel.findOneAndDelete({doctor: req.body.mobile_no}, (err, docs) => {
 				if (err) {
-					res.status(BAD_REQUEST).send("No referreral in this mobile no");
+					res.status(BAD_REQUEST).send("No referreral found for this mobile no");
 				} else {
 					var new_doctor = new doctorModel();
 					new_doctor.name = req.body.name;
@@ -26,6 +26,7 @@ exports.registration = async function (req, res) {
 					new_doctor.referrer = docs.referrer;
 
 					const payload = {
+						mobile_no: new_doctor.mobile_no,
 						name: new_doctor.name,
 						reg_number: new_doctor.reg_number,
 					};
@@ -37,13 +38,13 @@ exports.registration = async function (req, res) {
 						parseInt(process.env.SALT_ROUNDS, 10),
 						(err, hash) => {
 							if (err) {
-								res.status(INTERNAL_SERVER_ERROR).send("something went wrong");
+								res.status(INTERNAL_SERVER_ERROR).send("Something went wrong");
 							} else {
 								new_doctor.password = hash;
 								new_doctor.save((err, docs) => {
 									if (err) {
 										res.status(INTERNAL_SERVER_ERROR).send(
-											"something went wrong"
+											"Something went wrong"
 										);
 									} else {
 										res.status(SUCCESS).send(token);
@@ -61,13 +62,13 @@ exports.registration = async function (req, res) {
 exports.login = async function (req, res) {
 	doctorModel.findOne({mobile_no: req.body.mobile_no}, (err, docs) => {
 		if (err) {
-			res.status(INTERNAL_SERVER_ERROR).send("something went wrong");
+			res.status(INTERNAL_SERVER_ERROR).send("Something went wrong");
 		} else if (!docs) {
-			res.status(DATA_NOT_FOUND).send("no such user found");
+			res.status(DATA_NOT_FOUND).send("No such user found");
 		} else {
 			bcrypt.compare(req.body.password, docs.password, (err, result) => {
 				if (err) {
-					res.status(INTERNAL_SERVER_ERROR).send("something went wrong");
+					res.status(INTERNAL_SERVER_ERROR).send("Something went wrong");
 				} else {
 					const payload = {
 						mobile_no: docs.mobile_no,
@@ -82,7 +83,7 @@ exports.login = async function (req, res) {
 						{session_token: token},
 						(err, docs) => {
 							if (err) {
-								res.status(INTERNAL_SERVER_ERROR).send("something went wrong");
+								res.status(INTERNAL_SERVER_ERROR).send("Something went wrong");
 							} else {
 								res.status(SUCCESS).send(token);
 							}
@@ -97,9 +98,9 @@ exports.login = async function (req, res) {
 exports.reference = async function (req, res) {
 	doctorModel.findOne({mobile_no: req.body.doctor}, (err, docs) => {
 		if (err) {
-			res.status(INTERNAL_SERVER_ERROR).send("Internal server error");
+			res.status(INTERNAL_SERVER_ERROR).send("Something went wrong");
 		} else if (docs) {
-			res.status(BAD_REQUEST).send("This doctor already registered in our database");
+			res.status(BAD_REQUEST).send("The doctor has already registered");
 		} else {
 			var new_reference = new referenceModel();
 			new_reference.referrer = req.body.referrer;
@@ -107,7 +108,7 @@ exports.reference = async function (req, res) {
 
 			new_reference.save((err, docs) => {
 				if (err) {
-					res.status(INTERNAL_SERVER_ERROR).send("Internal server error");
+					res.status(INTERNAL_SERVER_ERROR).send("Something went wrong");
 				} else {
 					res.status(SUCCESS).send("Successfully referred");
 				}

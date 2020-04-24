0fbd55a46f53371c81c2515d354dbe94d4940605
return-100 <imransci143@gmail.com>
2020-04-24 16:48:07 +0600

    change get appointment route

--- routes/patient.js
+++ routes/patient.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/patient/post/login", patientController.login);
 router.post("/patient/post/register", patientController.registration);
 router.post("/patient/post/appointment", patientMiddleware.middleware, patientController.postAppointment);
-router.post("/patient/get/appointment", patientMiddleware.middleware, patientController.getAppointment);
+router.get("/patient/get/appointment", patientMiddleware.middleware, patientController.getAppointment);
 router.post("/patient/add/transaction", patientMiddleware.middleware, patientController.addTransaction);
 
 module.exports = router;

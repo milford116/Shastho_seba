6d05baf2a2830125c1ce11be493ce81b2cd1902a
Waqar-107 <waqar.hassan866@gmail.com>
2020-05-09 02:17:19 +0600

    2 new validator added

--- validators/doctor.validator.js
+++ validators/doctor.validator.js
@@ -83,3 +83,49 @@ module.exports.referrer = (data) => {
 		isValid: checker.isEmpty(errors),
 	};
 };
+
+module.exports.profileEdit = (data) => {
+	let errors = {};
+
+	if (data.updates.name !== undefined && validator.isEmpty(data.updates.name)) {
+		errors.name = "name cannot be empty";
+	}
+
+	if (data.updates.institution !== undefined && validator.isEmpty(data.updates.institution)) {
+		errors.institution = "institution cannot be empty";
+	}
+
+	if (data.updates.designation !== undefined && validator.isEmpty(data.updates.designation)) {
+		errors.designation = "designation is required";
+	}
+
+	if (data.updates.email !== undefined && !validator.isEmail(data.updates.email)) {
+		errors.email = "invalid email";
+	}
+
+	if (data.updates.email !== undefined && !validator.isEmail(data.updates.email)) {
+		errors.email = "invalid email";
+	}
+
+	if (data.updates.specialization !== undefined && !checker.isArrayAndNotEmpty(data.updates.specialization)) {
+		errors.specialization = "specialization cannot be empty";
+	}
+
+	return {
+		errors,
+		isValid: checker.isEmpty(errors),
+	};
+};
+
+module.exports.token = (data) => {
+	let errors = {};
+
+	if (validator.isEmpty(data.mobile_no)) {
+		errors.mobile_no = "mobile number is required";
+	}
+
+	return {
+		errors,
+		isValid: checker.isEmpty(errors),
+	};
+};

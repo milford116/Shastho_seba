7448b0335f2db5562d9af27b9e08716e16553e7c
Waqar-107 <waqar.hassan866@gmail.com>
2020-05-09 21:14:19 +0600

    env added

--- middlewares/validator.middleware.js
+++ middlewares/validator.middleware.js
@@ -2,10 +2,14 @@ const {BAD_REQUEST} = require("../errors");
 
 module.exports = (validator) => {
 	return (req, res, next) => {
-		const {errors, isValid} = validator(req.body, req.params);
-		if (!isValid) {
-			return res.status(BAD_REQUEST).send(errors);
-		}
-		next();
+		console.log("value: ", process.env.validateInput);
+		if (process.env.validateInput === "1") {
+			console.log("validating");
+			const {errors, isValid} = validator(req.body, req.params);
+			if (!isValid) {
+				return res.status(BAD_REQUEST).send(errors);
+			}
+			next();
+		} else next();
 	};
 };
--- routes/doctor.js
+++ routes/doctor.js
@@ -23,43 +23,14 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage});
 
-// router.post("/doctor/post/login", validatorMiddleWare(doctorValidator.login), doctorController.login);
-// router.post("/doctor/post/register", validatorMiddleWare(doctorValidator.registration), doctorController.registration);
-// router.post("/doctor/post/reference", validatorMiddleWare(doctorValidator.referrer), doctorMiddleware.middleware, doctorController.reference);
-// router.post("/doctor/edit/profile", validatorMiddleWare(doctorValidator.profileEdit), doctorMiddleware.middleware, doctorController.editDoctor);
+router.post("/doctor/post/login", validatorMiddleWare(doctorValidator.login), doctorController.login);
+router.post("/doctor/post/register", validatorMiddleWare(doctorValidator.registration), doctorController.registration);
+router.post("/doctor/post/reference", validatorMiddleWare(doctorValidator.referrer), doctorMiddleware.middleware, doctorController.reference);
+router.post("/doctor/edit/profile", validatorMiddleWare(doctorValidator.profileEdit), doctorMiddleware.middleware, doctorController.editDoctor);
 
-// router.post("/doctor/post/schedule", validatorMiddleWare(doctorValidator.postSchedule), doctorMiddleware.middleware, scheduleController.addSchedule);
-// router.get("/doctor/get/schedule", doctorMiddleware.middleware, scheduleController.getSchedule);
-// router.post("/doctor/edit/schedule", validatorMiddleWare(doctorValidator.editSchedule), doctorMiddleware.middleware, scheduleController.editSchedule);
-
-// router.post("/doctor/update/appointment", doctorMiddleware.middleware, appointmentController.updateAppointment);
-// router.get("/doctor/get/appointment", doctorMiddleware.middleware, appointmentController.appointment);
-// router.get("/doctor/get/futureAppointment", doctorMiddleware.middleware, appointmentController.getFutureAppointment);
-// router.get("/doctor/get/appointmentDetail", doctorMiddleware.middleware, appointmentController.appointmentDetail);
-
-// router.get("/doctor/search/name/:name/:limit/:page", searchController.searchByName);
-// router.get("/doctor/search/hospital_name/:hospital_name/:limit/:page", searchController.searchByHospital);
-// router.get("/doctor/search/specialization/:speciality/:limit/:page", searchController.searchBySpecialization);
-
-// router.get("/doctor/list/all/:limit/:page", doctorController.doctorList);
-
-// router.post("/doctor/get/transaction", doctorMiddleware.middleware, transactionController.getTransaction);
-
-// router.post("/doctor/get/token", validatorMiddleWare(doctorValidator.token), doctorMiddleware.middleware, tokenController.getToken);
-
-// router.post("/doctor/save/prescription", doctorMiddleware.middleware, upload.single("file"), validatorMiddleWare(doctorValidator.postPrescription), prescriptionController.postPrescription);
-// router.post("/doctor/get/prevPrescription", validatorMiddleWare(doctorValidator.getPreviousPrescriptions), doctorMiddleware.middleware, prescriptionController.getPreviousPrescription);
-
-//---------------------
-// validator nai
-router.post("/doctor/post/login", doctorController.login);
-router.post("/doctor/post/register", doctorController.registration);
-router.post("/doctor/post/reference", doctorMiddleware.middleware, doctorController.reference);
-router.post("/doctor/edit/profile", doctorMiddleware.middleware, doctorController.editDoctor);
-
-router.post("/doctor/post/schedule", doctorMiddleware.middleware, scheduleController.addSchedule);
+router.post("/doctor/post/schedule", validatorMiddleWare(doctorValidator.postSchedule), doctorMiddleware.middleware, scheduleController.addSchedule);
 router.get("/doctor/get/schedule", doctorMiddleware.middleware, scheduleController.getSchedule);
-router.post("/doctor/edit/schedule", doctorMiddleware.middleware, scheduleController.editSchedule);
+router.post("/doctor/edit/schedule", validatorMiddleWare(doctorValidator.editSchedule), doctorMiddleware.middleware, scheduleController.editSchedule);
 
 router.post("/doctor/update/appointment", doctorMiddleware.middleware, appointmentController.updateAppointment);
 router.get("/doctor/get/appointment", doctorMiddleware.middleware, appointmentController.appointment);
@@ -74,11 +45,9 @@ router.get("/doctor/list/all/:limit/:page", doctorController.doctorList);
 
 router.post("/doctor/get/transaction", doctorMiddleware.middleware, transactionController.getTransaction);
 
-router.post("/doctor/get/token", doctorMiddleware.middleware, tokenController.getToken);
+router.post("/doctor/get/token", validatorMiddleWare(doctorValidator.token), doctorMiddleware.middleware, tokenController.getToken);
 
-router.post("/doctor/save/prescription", doctorMiddleware.middleware, upload.single("file"), prescriptionController.postPrescription);
-router.post("/doctor/get/prevPrescription", doctorMiddleware.middleware, prescriptionController.getPreviousPrescription);
-
-//---------------------
+router.post("/doctor/save/prescription", doctorMiddleware.middleware, upload.single("file"), validatorMiddleWare(doctorValidator.postPrescription), prescriptionController.postPrescription);
+router.post("/doctor/get/prevPrescription", validatorMiddleWare(doctorValidator.getPreviousPrescriptions), doctorMiddleware.middleware, prescriptionController.getPreviousPrescription);
 
 module.exports = router;

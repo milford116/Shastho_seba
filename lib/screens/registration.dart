743de4980ec896b8f98d1ae818694f3c6cd14852
fuad113 <anfuad96@yahoo.com>
2020-07-29 21:33:02 +0600

    registration page done

---
+++ lib/screens/registration.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+import '../utils.dart';
+import '../routes.dart';
+
+class Registration extends StatefulWidget {
+  @override
+  _RegistrationState createState() => _RegistrationState();
+}
+
+class _RegistrationState extends State<Registration> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('images/patient_background_low_opacity.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: lightBlue,
+          centerTitle: true,
+          title: Text('Registration'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+          child: ListView(
+            children: <Widget>[
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Name',
+                        icon: Icon(Icons.person),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12.0),
+                          child: Text(
+                            'Gender:',
+                            style: M,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Row(
+                              children: <Widget>[
+                                Radio(
+                                  value: 'male',
+                                ),
+                                Text(
+                                  'Male',
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: <Widget>[
+                                Radio(
+                                  value: 'female',
+                                ),
+                                Text(
+                                  'Female',
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Mobile No',
+                        icon: Icon(Icons.phone_android),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        labelText: 'Date of Birth',
+                        suffixIcon: Icon(Icons.date_range),
+                        icon: Opacity(
+                          child: Icon(Icons.phone_android),
+                          opacity: 0.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        icon: Icon(Icons.lock),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Confirm Password',
+                        icon: Icon(Icons.lock),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                    ),
+                    FlatButton(
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      color: blue,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

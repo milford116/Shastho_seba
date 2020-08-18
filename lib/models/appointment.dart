455c37db6ea47a5faaf659916e8717265510b9bf
Zangetsu101 <haidertameem@gmail.com>
2020-08-18 21:44:21 +0600

    appointment model updated

--- lib/models/appointment.dart
+++ lib/models/appointment.dart
@@ -1,10 +1,19 @@
+enum AppointmentStatus { NoPayment, NotVerified, Verified, Finished }
+
+Map<int, AppointmentStatus> map = {
+  0: AppointmentStatus.NoPayment,
+  1: AppointmentStatus.NotVerified,
+  2: AppointmentStatus.Verified,
+  3: AppointmentStatus.Finished,
+};
+
 class Appointment {
   final String id;
   final String doctorMobileNo;
   final String patientMobileNo;
   final String doctorName;
   final DateTime dateTime;
-  final int status;
+  final AppointmentStatus status;
   final int serialNo;
   final String imageURL;
 
@@ -23,7 +32,7 @@ class Appointment {
         doctorMobileNo = json['doc_mobile_no'],
         patientMobileNo = json['patient_mobile_no'],
         doctorName = json['doctorId']['name'],
-        status = json['status'],
+        status = map[json['status']],
         serialNo = json['serial_no'],
         dateTime = DateTime.parse(json['appointment_date_time']),
         imageURL = json['prescription_img'];

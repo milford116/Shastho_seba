76179156e3a2b61c9acdfd67d1dbef4903268521
Zangetsu101 <haidertameem@gmail.com>
2020-09-02 09:49:57 +0600

    app exit confirmation

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,18 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 
 import '../utils.dart';
 import '../routes.dart';
 import '../widgets/drawer.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _backPressed = false;
+  Timer _timer;
   @override
   Widget build(BuildContext context) {
     String message = ModalRoute.of(context).settings.arguments;
@@ -49,39 +57,42 @@ class HomeScreen extends StatelessWidget {
                   ),
                 );
               }
-              return Center(
-                child: GridView.count(
-                  // padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  childAspectRatio: 1.5,
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  children: <Widget>[
-                    _Tile(
-                      title: 'Appointments Today ',
-                      icon: Icons.schedule,
-                      route: appointmentsTodayScreen,
-                    ),
-                    _Tile(
-                      title: 'Find Doctors',
-                      icon: Icons.search,
-                      route: findDoctorsScreen,
-                    ),
-                    _Tile(
-                      title: 'Appointments',
-                      icon: Icons.insert_invitation,
-                      route: appointmentsScreen,
-                    ),
-                    _Tile(
-                      title: 'Profile',
-                      icon: Icons.person,
-                      route: profileScreen,
-                    ),
-                    _Tile(
-                      title: 'Feedback',
-                      icon: Icons.feedback,
-                      route: feedbackScreen,
-                    ),
-                  ],
+              return WillPopScope(
+                onWillPop: () => _exitApp(context),
+                child: Center(
+                  child: GridView.count(
+                    // padding: EdgeInsets.symmetric(horizontal: 20.0),
+                    childAspectRatio: 1.5,
+                    shrinkWrap: true,
+                    crossAxisCount: 2,
+                    children: <Widget>[
+                      _Tile(
+                        title: 'Appointments Today ',
+                        icon: Icons.schedule,
+                        route: appointmentsTodayScreen,
+                      ),
+                      _Tile(
+                        title: 'Find Doctors',
+                        icon: Icons.search,
+                        route: findDoctorsScreen,
+                      ),
+                      _Tile(
+                        title: 'Appointments',
+                        icon: Icons.insert_invitation,
+                        route: appointmentsScreen,
+                      ),
+                      _Tile(
+                        title: 'Profile',
+                        icon: Icons.person,
+                        route: profileScreen,
+                      ),
+                      _Tile(
+                        title: 'Feedback',
+                        icon: Icons.feedback,
+                        route: feedbackScreen,
+                      ),
+                    ],
+                  ),
                 ),
               );
             },
@@ -89,6 +100,22 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<bool> _exitApp(BuildContext context) async {
+    if (_backPressed) {
+      _timer.cancel();
+      return true;
+    }
+    _backPressed = true;
+    _timer = Timer(Duration(seconds: 2), () => _backPressed = false);
+    Scaffold.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Press back again to exit'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+    return false;
   }
 }
 
